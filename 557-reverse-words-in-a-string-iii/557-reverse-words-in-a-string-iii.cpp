6d73e7b4402fb1d947f0d6b3f85f7b363204a16f
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2022-06-08 09:50:57 +0530

    Time: 18 ms (59.47%), Space: 9.6 MB (90.03%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        
+        int left=0, right=0;
+        while(left< s.size())
+        {
+            while(right< s.size() && s[right]!=' ')
+                right++;
+        reverse(s.begin()+left, s.begin()+right);
+            left= right+1;
+            right=left;
+        }
+        
+        return s;
+    }
+};
