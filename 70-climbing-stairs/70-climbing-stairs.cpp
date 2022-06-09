e6676f66a6477ba27b91ac62cbcc1fa7821a7ba4
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2022-06-09 19:42:45 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (56.38%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2)
+            return n;
+        int x=1; int y=2;
+        for(int i=3; i<n; i++)
+        {
+            int temp=y;
+            y+=x;
+            x=temp;
+        }
+        return x+y;
+        
+    }
+};
