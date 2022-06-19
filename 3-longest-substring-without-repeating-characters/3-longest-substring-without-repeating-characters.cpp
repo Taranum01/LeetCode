fad8bff070659204c9ed2bfbe7eff76385e0530e
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2022-06-19 17:01:28 +0530

    Time: 10 ms (84.17%), Space: 8.2 MB (81.47%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,48 @@
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        int n=s.size(),l=0,r=0,ans=0;     
         
-        vector<int> mp(256, -1); 
+        vector<int> count(256,0);      
         
-        int left=0;
-        int right=0;
-        int n= s.size();
-        int len=0;
-        
-        while(right<n){
-            if(mp[s[right]!= -1])
-            left= max(mp[s[right]]+1,left);
-            mp[s[right]]=right;
-            len= max(len, right-left + 1);
-            right++;
-    }
-    return len;
+        while(r<n)      
+        {
+            count[s[r]]++;     
+            
+            while(count[s[r]]>1)      // the char is repeated
+            {
+                count[s[l]]--;     //reduce the occurence of char 
+                
+                l++;       //contraction of the present window 
+            }
+            ans=max(ans,r-l+1);    
+            
+            r++;     
+        }
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+        
+//         vector<int> mp(256, -1); 
+        
+//         int left=0;
+//         int right=0;
+//         int n= s.size();
+//         int len=0;
+        
+//         while(right<n){
+//             if(mp[s[right]!= -1])
+//             left= max(mp[s[right]]+1,left);
+//             mp[s[right]]=right;
+//             len= max(len, right-left + 1);
+//             right++;
+//     }
+//     return len;
+//     }
+// };
+
