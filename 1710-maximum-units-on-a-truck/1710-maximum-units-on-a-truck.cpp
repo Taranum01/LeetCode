b650e3bfde2d832875d0b9c3367d1df2c66b74e6
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2022-07-01 11:41:05 +0530

    Time: 324 ms (12.98%), Space: 49.4 MB (14.24%) - LeetHub

---
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    // comparator function to sort in vector w.r.t no. of units per boxes in descending order
+    static bool compare(vector<int> v1, vector<int> v2)
+    {
+        return v1[1]>v2[1];
+    }
+    
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        
+        int maxUnits = 0, i = 0;
+        
+        sort(boxTypes.begin(), boxTypes.end(), compare);
+        
+        while (truckSize>0 && i<boxTypes.size()){
+            
+            if(boxTypes[i][0] <= truckSize){
+                maxUnits += boxTypes[i][0] * boxTypes[i][1];
+                truckSize -= boxTypes[i][0];
+            }
+    
+            else{
+                
+                int boxesLeft = truckSize;
+                truckSize -= boxesLeft; // here truckSize = 0 
+                maxUnits += boxesLeft * boxTypes[i][1]; 
+            }
+            
+            i++;
+        }
+    
+        return maxUnits;
+    }
+};
