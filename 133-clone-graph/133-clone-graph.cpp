7c357d13508ab799b20d16797523447dcedab019
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2022-07-02 18:45:56 +0530

    Time: 9 ms (56.50%), Space: 9.2 MB (10.27%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+     
+    void dfs(Node* node, Node* copy, vector<Node*>&visited){
+        
+        visited[copy->val]= copy;
+        for(auto x: node->neighbors){
+            if(visited[x->val]==NULL){
+                Node* newnode= new Node(x->val);
+                (copy->neighbors).push_back(newnode);
+                dfs(x,newnode,visited);
+            }
+            else{
+                (copy->neighbors).push_back(visited[x->val]);
+            }
+        }
+    }
+    
+    Node* cloneGraph(Node* node) {
+       
+       if(node==NULL)
+           return NULL;
+       vector<Node*>visited(1000,NULL);
+       Node* copy= new Node (node->val);
+       dfs(node, copy, visited);
+       return copy;
+    }
+};
