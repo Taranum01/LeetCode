f3de3f2a97c96b47ddce0babba23594c7404de52
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2022-07-05 12:22:56 +0530

    Time: 1172 ms (10.10%), Space: 49.7 MB (52.56%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,27 +1,58 @@
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         set<int>hashSet;
+//         for(int num:nums)
+//         {
+//             hashSet.insert(num);
+//         }
+//         int longestSeq=0;
+//         for(int num:nums){
+//             if(!hashSet.count(num-1))
+//             {
+//                 int currNum= num;
+//                 int currSeq= 1;
+            
+//             while(hashSet.count(currNum+1)){
+//                 currNum+=1;
+//                 currSeq+=1;
+//             }
+            
+//             longestSeq= max(longestSeq, currSeq);
+//         }
+        
+//     }
+//     return longestSeq;
+//     }
+// };
+
+#include<unordered_set>
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        set<int>hashSet;
-        for(int num:nums)
-        {
-            hashSet.insert(num);
-        }
-        int longestSeq=0;
-        for(int num:nums){
-            if(!hashSet.count(num-1))
-            {
-                int currNum= num;
-                int currSeq= 1;
-            
-            while(hashSet.count(currNum+1)){
-                currNum+=1;
-                currSeq+=1;
-            }
-            
-            longestSeq= max(longestSeq, currSeq);
+        unordered_set<int> s;
+        for(int i=0;i<nums.size();i++){
+            s.insert(nums[i]);
         }
         
-    }
-    return longestSeq;
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            if(s.find(nums[i]-1)!=s.end()){
+                continue;
+            
+            }else{
+                int longestSeq=0;
+                int current=nums[i];
+                while(s.find(current)!=s.end()){
+                    longestSeq++;
+                    current++;
+                }
+                
+                ans=max(ans,longestSeq);
+            } 
+        }
+        return ans;
+        
     }
 };
+
