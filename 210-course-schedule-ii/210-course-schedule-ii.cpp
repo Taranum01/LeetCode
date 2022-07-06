262019e6d2bad5b02e1be4b877f21e7c0adffaab
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2022-07-06 18:05:47 +0530

    Time: 54 ms (13.08%), Space: 13.5 MB (81.76%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    
+    bool kahnsAlgo(vector<vector<int>>&adj, int n, vector<int>&inDegree, vector<int>&ans){
+        queue<int> q;
+	    for (int i = 0; i < n; i++)
+		if (inDegree[i] == 0)
+			q.push(i);
+        
+        int count=0;
+        while (!q.empty())
+	    {
+		int curr = q.front();
+		q.pop();
+            for (auto a : adj[curr])
+		{
+			inDegree[a]--;
+			if (inDegree[a] == 0)
+				q.push(a);
+		}
+            ans.push_back(curr);
+            count++;
+        }
+        
+        if (count != n)
+		   return false;
+	    return true;
+}
+    
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        int n= prerequisites.size();
+        vector<vector<int>>adj(numCourses);
+        vector<int>inDegree(numCourses,0);
+        
+        for (int i = 0; i < n; i++)
+	    {
+		adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+		inDegree[prerequisites[i][0]]++;
+	    }
+        
+        vector<int>ans;
+
+	    if (kahnsAlgo(adj, numCourses, inDegree, ans))
+		return ans;
+        
+        vector<int> empty;
+	    return empty;
+    }
+};
+
