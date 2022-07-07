eb0864eadbcc793c07d55ba7cc75f88b3f905e9c
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2022-07-07 23:33:04 +0530

    Time: 82 ms (15.28%), Space: 35.2 MB (13.32%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    bool helper(string s1,string s2, string s3, int i, int j, int k, vector<vector<int>> &v)
+{
+        
+        if(v[i][j]!=-1){
+            return v[i][j];
+        }
+        
+        if(i==s1.length() && j==s2.length() && k==s3.length()){
+            return true;
+        }
+        
+        bool x=false, y=false;
+        
+        if(i!=s1.length()){
+            
+            if(s1[i]==s3[k]){
+                x = helper(s1, s2, s3, i+1, j, k+1, v);
+            }   
+        }
+        if(j!=s2.length()){
+            
+            if(s2[j]==s3[k]){
+                y = helper(s1, s2, s3, i, j+1, k+1, v);
+            }   
+        }
+        
+        return v[i][j]= x||y;
+    }
+    
+    bool isInterleave(string s1, string s2, string s3) {
+        
+        int a=s1.length(),b=s2.length();
+        vector<vector<int>> v(a+1,vector<int>(b+1,-1));
+        int i=0,j=0,k=0;
+        return helper(s1, s2, s3, 0, 0, 0, v);
+        
+    }
+};
