8f4de4ea53ff9f98dccf6b68562e41ed9fb402f0
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2022-07-14 18:33:39 +0530

    Time: 4 ms (75.18%), Space: 7.3 MB (66.61%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        
+        int n=s.length();
+        
+        stack<int>st;
+        
+        st.push(-1);
+        
+        int ans=0;
+        
+        for(int i=0; i<n; i++){
+            
+            if(s[i]=='(')
+                st.push(i);
+            
+            else{
+                
+                if(!st.empty())
+                    st.pop();
+                
+                if(!st.empty())
+                    ans= max(ans, i-st.top());
+                else{
+                    st.push(i);
+                }
+            }
+        }
+            return ans;
+    }
+};
