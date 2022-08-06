831a2479a0926acb8b319cea27935bdec2a63258
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2022-08-07 01:49:05 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (22.55%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        
+        int trial=minutesToTest/minutesToDie;
+        return ceil (log(buckets)/log(trial+1));
+        
+    }
+};
