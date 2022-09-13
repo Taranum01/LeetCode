02078a1c63d66f365e921612bb514db21ce3d67d
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2022-09-14 04:56:53 +0530

    Time: 732 ms (5.07%), Space: 120.3 MB (9.86%) - LeetHub

---
+++ 575-distribute-candies/575-distribute-candies.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        
+        int n=candyType.size();
+        set<int>s;
+        for(auto i: candyType)
+            s.insert(i);
+        int m=s.size();
+        
+        return min(n/2,m);
+    }
+};
