0d05d4724dbad446fcf2ee9538e9e933a3efa18d
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2022-09-14 05:36:03 +0530

    Time: 431 ms (5.03%), Space: 32 MB (81.36%) - LeetHub

--- 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.cpp
+++ 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
         
-        int n1 = list1.size();
-        int n2 = list2.size();
+        int s1 = list1.size();
+        int s2 = list2.size();
         
         int mini = INT_MAX;
         
         unordered_map<int, vector<string>> mp;
         
-        for(int i = 0; i < n1; i++)
+        for(int i=0; i<s1; i++)
         {
-            for(int j = 0; j < n2; j++)
+            for(int j=0; j<s2; j++)
             {
                 if(list1[i] == list2[j])
                 {
-                    mini = min(mini, i + j);
-                    mp[i + j].push_back(list1[i]);
+                    mini = min(mini, i+j);
+                    mp[i+j].push_back(list1[i]);
                 }
             }
         }
