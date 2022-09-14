c1c3297405d70a0e50126024f755077d264ec95f
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2022-09-14 06:32:18 +0530

    Time: 443 ms (8.50%), Space: 73.1 MB (9.71%) - LeetHub

---
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    int lcs(string text1, string text2) {
+        int a= text1.size();
+        int b= text2.size();
+        vector<vector<int>>dp(a+1, vector<int>(b+1,0));
+        
+        for(int i=0;i<=a;i++) dp[i][0]=0;
+        for(int j=0;j<=b;j++) dp[0][j]=0;
+        
+        for(int i=1;i<=a;i++) 
+        {
+        for(int j=1;j<=b;j++) {
+            
+            if(text1[i-1]==text2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+            else
+                dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
+        
+        }
+            
+        }
+        
+        return dp[a][b];
+    
+    }
+    int longestPalindromeSubseq(string s) {
+        
+        string t=s;
+        reverse(t.begin(), t.end());
+        return lcs(s,t);
+    }
+};
