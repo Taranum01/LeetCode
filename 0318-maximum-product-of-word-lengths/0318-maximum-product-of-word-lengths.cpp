a23281e1c9ef9ac0c2bc8b77f806889425b5bb21
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2022-11-29 01:34:55 +0530

    Time: 173 ms (67.03%), Space: 21.8 MB (64.41%) - LeetHub

---
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        
+        vector<int>state(words.size());
+        int ans=0;
+        
+        for(int i=0; i<words.size(); i++){
+            for(char c: words[i]){
+                state[i] |= 1<<(c-'a');
+            }
+            
+            for(int j=0; j<i; j++){
+                if(!(state[i]&state[j])) {
+                if(words[i].size()*words[j].size()>ans) 
+                    ans= words[i].size()*words[j].size();
+                }
+            }
+        }
+        return ans;
+    }
+};
