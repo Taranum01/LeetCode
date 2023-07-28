cace14adbf38549bb8dba50ba881424514527d46
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2023-07-28 13:37:03 +0530

    Time: 0 ms (100%), Space: 7.3 MB (75.78%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -3,38 +3,27 @@ public:
 
     int n;
     int dp[23][23];
-
-    int solve(vector<int>& nums, int l, int r) {
+    
+  
+    int maxDiff(vector<int>& nums, int l, int r) {
         
-        if(l>r)
-            return 0;
-        
-        if(l==r)
+        if(l == r)
             return nums[l];
         
         if(dp[l][r] != -1)
             return dp[l][r];
         
-        int take_left  = nums[l] + min(solve(nums, l+2, r), solve(nums, l+1, r-1));
-        
-        int take_right = nums[r] + min(solve(nums, l, r-2), solve(nums, l+1, r-1));
-        
+        int take_left  = nums[l] - maxDiff(nums, l+1, r);
+        int take_right = nums[r] - maxDiff(nums, l, r-1);
         
         return dp[l][r] = max(take_left, take_right);
-        
     }
     
     bool PredictTheWinner(vector<int>& nums) {
-        
-        memset(dp, -1, sizeof(dp));
-        
+
         n = nums.size();
-        
-        int total = accumulate(begin(nums), end(nums), 0);
-        
-        int player1 = solve(nums, 0, n-1);
-        int player2 = total - player1;
-        
-        return player1 >= player2;
+        memset(dp, -1, sizeof(dp));
+        return maxDiff(nums, 0, n-1) >= 0;
+            
     }
 };
