16c96fff58981d72dc0358f8833f9914ecd40f58
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2023-08-06 22:25:27 +0530

    Time: 3 ms (83.78%), Space: 6.2 MB (61.26%) - LeetHub

---
+++ 0956-number-of-music-playlists/0956-number-of-music-playlists.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+
+    int N;
+    int GOAL;
+    int K;
+    long dp[101][101];
+    const int MOD = 1e9+7;
+    
+    long solve(int count_song, int count_unique) {
+
+        if(count_song == GOAL) {
+            if(count_unique == N)
+                return 1;
+            
+            return 0;
+        }
+        
+        if(dp[count_song][count_unique] != -1)
+            return dp[count_song][count_unique];
+        
+        long res = 0;
+        
+        if(count_unique < N)
+            res += (N-count_unique) * solve(count_song+1, count_unique+1);
+        
+        if(count_unique > K)
+            res += (count_unique - K) * solve(count_song+1, count_unique);
+        
+        
+        return dp[count_song][count_unique] = res % MOD;
+
+    }
+    
+    int numMusicPlaylists(int n, int goal, int k) {
+
+        N    = n;
+        GOAL = goal;
+        K    = k;
+        memset(dp, -1, sizeof(dp));
+        return solve(0, 0); 
+        
+    }
+};
