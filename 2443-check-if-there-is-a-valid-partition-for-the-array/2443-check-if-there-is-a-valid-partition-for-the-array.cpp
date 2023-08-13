20e45bdd08281ac5ff8df0c8623571ba1e11a47c
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2023-08-13 10:20:47 +0530

    Time: 125 ms (91.38%), Space: 92.2 MB (37.85%) - LeetHub

---
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/2443-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool validPartition(vector<int>& nums) {
+        
+        vector<int>dp(nums.size(), -1);
+        return func(nums, 0, dp);
+    }
+        bool func(vector<int> &nums, int i, vector<int>&dp){
+            
+            if(i==nums.size())
+                return true;
+            if(dp[i]!=-1) return dp[i];
+            
+            if(i+1<nums.size() && nums[i+1]==nums[i]){
+                
+                if(func(nums, i+2, dp)) return dp[i]= true;
+                
+                if(i+2<nums.size() && nums[i+2]==nums[i]){
+                    
+                if(func(nums, i+3, dp)) return dp[i]= true;   
+            }      
+        }
+            
+        if(i+2<nums.size() && nums[i+1]==nums[i]+1 && nums[i+2]== nums[i]+2)
+        {
+            if(func(nums, i+3, dp)) return dp[i]= true;
+        }
+            return dp[i]= false;
+    }
+};
