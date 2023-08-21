fac52cd730a33d45f71d3a2a6ff0730523e4bebf
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2023-08-21 19:37:58 +0530

    Time: 49 ms (29.29%), Space: 91.7 MB (20.17%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    
-    bool isSafe(string s, string str){
-        
-        for(int i=0; i<s.length(); i+=str.length()){
-        
-            string curr= s.substr(i,str.length());
-            if(curr!=str)
-            return false;
-        }
-        return true;
-        
-    }
     bool repeatedSubstringPattern(string s) {
-        
-        string str="";
-        
-        for(int i=0; i<s.length()/2; i++){
-            
-            str+=s[i];
-            if(s.length()%str.length()==0)
-            {
-                
-            bool x= isSafe(s.substr(i+1), str);
-            if(x) 
-                return true;
-            }
-          }
 
+        int n = s.length();
+
+        for(int l = 1; l <= n/2; l++) {
+            
+            if(n % l == 0) {
+
+                int count = n/l;
+                
+                string pattern   = s.substr(0, l);
+                string newStr = "";
+
+                while(count--) {
+                    
+                    newStr += pattern;
+                }
+                
+                if(newStr == s)
+                    return true;
+            }
+            
+        }
+        
         return false;
     }
 };
