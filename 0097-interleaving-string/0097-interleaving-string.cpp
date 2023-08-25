e02516c34c1afeec3c9bca85663495b73f3d40a2
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2023-08-25 13:10:50 +0530

    Time: 53 ms (16.41%), Space: 35.1 MB (19.48%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
-    
-    bool helper(string s1,string s2, string s3, int i, int j, int k, vector<vector<int>> &v)
-{
-        
-        if(v[i][j]!=-1){
-            return v[i][j];
-        }
-        
-        if(i==s1.length() && j==s2.length() && k==s3.length()){
+
+    int m, n, N;
+
+    int dp[101][101][201];
+
+    bool check(string s1, string s2, string s3, int i, int j, int k) {
+
+        if(i >= m && j >= n && k >= N)\
             return true;
-        }
         
-        bool x=false, y=false;
-        
-        if(i!=s1.length()){
+        if(k >= N) 
+            return false;
             
-            if(s1[i]==s3[k]){
-                x = helper(s1, s2, s3, i+1, j, k+1, v);
-            }   
-        }
-        if(j!=s2.length()){
-            
-            if(s2[j]==s3[k]){
-                y = helper(s1, s2, s3, i, j+1, k+1, v);
-            }   
-        }
+        if(dp[i][j][k] != -1)
+            return dp[i][j][k];
         
-        return v[i][j]= x||y;
+        return dp[i][j][k] = ((s1[i]==s3[k] && 
+        check(s1, s2, s3, i+1, j, k+1)) || 
+        (s2[j] == s3[k] && check(s1, s2, s3, i, j+1, k+1)));
     }
-    
+
     bool isInterleave(string s1, string s2, string s3) {
+
+        memset(dp, -1, sizeof(dp));
+
+        m = s1.length();
+        n = s2.length();
+        N = s3.length();
         
-        int a=s1.length(),b=s2.length();
-        vector<vector<int>> v(a+1,vector<int>(b+1,-1));
-        int i=0,j=0,k=0;
-        return helper(s1, s2, s3, 0, 0, 0, v);
-        
+        return check(s1, s2, s3, 0, 0, 0);
     }
 };
