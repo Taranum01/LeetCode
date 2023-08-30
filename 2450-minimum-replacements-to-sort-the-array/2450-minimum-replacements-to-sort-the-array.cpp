f3555b9d537d6aa5e8378478d9e38ef914dad6c9
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2023-08-30 15:25:59 +0530

    Time: 98 ms (59.46%), Space: 54.5 MB (87.69%) - LeetHub

---
+++ 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long minimumReplacement(vector<int>& nums) {
+
+        int n = nums.size();
+        long long res = 0;
+        
+        for(int i=n-2; i>=0; i--) {
+
+            int splits = nums[i]/nums[i+1];
+            
+            if((nums[i]) % nums[i+1] != 0) {
+                splits++;
+            }
+            
+            nums[i] = nums[i]/splits;
+            res += splits-1;
+        }
+        
+        return res;
+    }
+};
