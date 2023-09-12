86a6765f425a0036886b0d1135be0101a09d431f
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2023-09-12 17:53:54 +0530

    Time: 43 ms (92.49%), Space: 17.1 MB (92.4%) - LeetHub

--- 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -2,23 +2,30 @@ class Solution {
 public:
     int minDeletions(string s) {
 
-        unordered_set<int> st;
         int freq[26] = {0};
-      
+        
         for(char &ch : s) {
             freq[ch-'a']++;
         }
+
+        sort(begin(freq), end(freq));
         
         int res = 0;
-        for(int i=0; i<26; i++) {
-            
-            while(freq[i] > 0 && st.find(freq[i]) != st.end()) {
-                freq[i]--;
-                res++;
-            }
-            st.insert(freq[i]);
-        }
         
+        for(int i=24; i>=0 && freq[i]>0; i--) {
+            
+            if(freq[i] >= freq[i+1]) {
+                
+                int prev = freq[i];
+                
+                freq[i] = max(0, freq[i+1]-1);
+                
+                res += (prev - freq[i]);
+                
+            }
+            
+        }
+          
         return res;
     }
 };
