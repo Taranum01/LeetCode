589ed8487d7580551d82095250b68e57f44a2b2a
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2023-09-17 13:28:00 +0530

    Time: 104 ms (36.71%), Space: 17.8 MB (45.73%) - LeetHub

---
+++ 0877-shortest-path-visiting-all-nodes/0877-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    
+    typedef pair<int, int> P;
+    
+    int shortestPathLength(vector<vector<int>>& graph) {
+
+        int n=graph.size();
+        
+        if(n == 1 || n == 0)
+            return 0;
+        
+        queue<P> que;
+        
+        set<P> vis;
+     
+        int allVisitedState = (1 << n) - 1;
+            
+        for(int i=0; i<n; i++) {
+
+            int maskValue = (1 << i);
+            
+            que.push({i, maskValue});
+            
+            vis.insert({i, maskValue});
+        }
+        
+        int res = 0;
+        
+        while(!que.empty())  {
+            
+            int N = que.size();
+            res++;
+            while(N--) {
+                
+                auto curr = que.front();
+                que.pop();
+                
+                int currNode = curr.first;
+                int currMask = curr.second;
+                
+                for(auto &adj: graph[currNode]) {
+                    int nextMask = currMask | (1 << adj);
+                    
+                    if(nextMask == allVisitedState)
+                        return res;
+                    
+                    if(vis.find({adj, nextMask}) == vis.end()) {
+                        vis.insert({adj, nextMask});
+                        que.push({adj, nextMask});                      
+                    }
+                }  
+            }
+        }
+
+        return -1;
+    }
+};
