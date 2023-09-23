4b94f7199c4ebb2bf010864fbd45e3c983731bd0
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2023-09-23 10:31:25 +0530

    Time: 147 ms (39.29%), Space: 51.8 MB (31.77%) - LeetHub

---
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestStrChain(vector<string>& words) {
+        
+        unordered_map<string,int>dp;
+        
+        int ans=0;
+        
+        sort(words.begin(), words.end(), sortbylen);
+        
+        for(string word: words){
+            
+            for(int i=0; i<word.length(); i++){
+                
+            string pre= word.substr(0,i)+word.substr(i+1);
+            dp[word]=max(dp[word], dp[pre]+1);
+            
+            }
+            
+            ans= max(ans, dp[word]);
+        }
+        return ans;
+    }
+    
+    static bool sortbylen(string &s1, string &s2){
+        
+        return s1.length()<s2.length();
+    }
+};
