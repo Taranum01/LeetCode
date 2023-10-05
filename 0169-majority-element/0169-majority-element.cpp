aa00a7b436af0dd778cc7b743e9363729866a000
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2023-10-05 12:17:00 +0530

    Time: 7 ms (97.64%), Space: 19.9 MB (28.43%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0;
+        int element=0;
+        
+        for(int n: nums){
+            if (count==0)
+            {
+            element= n;
+        }
+        if(n== element) count++;
+            else count--;
+            
+        }
+    return element;
+    }
+};
