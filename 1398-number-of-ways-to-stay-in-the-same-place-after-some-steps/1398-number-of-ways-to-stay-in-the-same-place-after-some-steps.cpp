b72663cb3d6c3d79893dc15f0d905d674bf869f9
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2023-10-15 16:48:12 +0530

    Time: 6 ms (96.54%), Space: 7.7 MB (79.44%) - LeetHub

---
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    int n;
+    int dp[501][501];
+    int MOD = 1e9+7;
+
+    int solve(int idx, int steps) {
+        
+        if(idx < 0 || idx >= n)
+            return 0;
+        
+        if(steps == 0) {
+            return idx == 0;
+        }
+        
+        if(dp[idx][steps] != -1)
+            return dp[idx][steps];
+        
+        int res = solve(idx+1, steps-1) % MOD; 
+
+        res = (res + solve(idx-1, steps-1)) % MOD; 
+
+        res = (res + solve(idx, steps-1)) % MOD; 
+        
+        return dp[idx][steps] = res;
+        
+    }
+    
+    int numWays(int steps, int arrLen) {
+        
+        arrLen = min(arrLen, steps);
+        
+        n = arrLen;
+
+        memset(dp, -1, sizeof(dp));
+
+        return solve(0, steps);
+    }
+};
