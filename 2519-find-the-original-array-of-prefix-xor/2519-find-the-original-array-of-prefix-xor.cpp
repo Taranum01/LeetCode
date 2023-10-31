1443c21db54f49ecfcd50622b4652d1462dc33b8
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2023-10-31 12:04:35 +0530

    Time: 97 ms (15.16%), Space: 76.1 MB (88.47%) - LeetHub

--- 2519-find-the-original-array-of-prefix-xor/2519-find-the-original-array-of-prefix-xor.cpp
+++ 2519-find-the-original-array-of-prefix-xor/2519-find-the-original-array-of-prefix-xor.cpp
@@ -4,14 +4,11 @@ public:
         
         int n = pref.size();
 
-        vector<int> arr;
-        arr.push_back(pref[0]);
-
-        for(int i = 1; i<n; i++) {
-            arr.push_back(pref[i] ^ pref[i-1]);
+        for(int i = n-1; i > 0; i--) {
+            pref[i] = pref[i] ^ pref[i-1];
         }
 
         
-        return arr;
+        return pref;
     }
 };
