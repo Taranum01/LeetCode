fb42b0899da441f10fc5a54f1d99627511bf1dd6
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2023-11-03 16:32:13 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.73%) - LeetHub

---
+++ 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+
+        int stream = 1;
+        
+        vector<string> res;
+        
+        int i = 0;
+
+        while(i < target.size() && stream <= n) {
+
+            res.push_back("Push");
+            
+            if(stream == target[i]) {
+                i++;
+            } else {
+                res.push_back("Pop");
+            }
+            stream++;
+        }
+        
+        return res;
+    }
+};
