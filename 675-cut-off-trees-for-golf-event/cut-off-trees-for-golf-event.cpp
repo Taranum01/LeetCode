0707494fdaf3258f1503c5f58eeb19dcb4d1fc48
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2025-02-03 22:51:35 +0530

    Time: 504 ms (92.09%) | Memory: 121.1 MB (92.60%) - LeetSync

--- 675-cut-off-trees-for-golf-event/cut-off-trees-for-golf-event.cpp
+++ 675-cut-off-trees-for-golf-event/cut-off-trees-for-golf-event.cpp
@@ -1,28 +1,29 @@
 class Solution {
 private:
-    const int MAX_DISTANCE = 1061109567;
-
+    const int MAX_D = 1061109567;
 public:
+
     int cutOffTree(vector<vector<int>>& forest) {
 
         int rows = forest.size();
         int cols = forest[0].size();
 
-        int totalDistance = 0;
+        int totalDist = 0;
 
-        set<pair<int, pair<int, int>>> treePositions;
+        set<pair<int, pair<int, int>>> treePos;
 
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
                 if (forest[r][c] > 1) {
-                    treePositions.insert({forest[r][c], {r, c}});
+                    treePos.insert({forest[r][c], {r, c}});
                 }
             }
         }
 
         int startX = 0, startY = 0;
 
-        for (auto& tree : treePositions) {
+        for (auto& tree : treePos) {
+
             int targetX = tree.second.first;
             int targetY = tree.second.second;
 
@@ -30,14 +31,14 @@ public:
 
             if (distanceToNextTree == -1) return -1;
 
-            totalDistance += distanceToNextTree;
+            totalDist += distanceToNextTree;
 
             forest[targetX][targetY] = 1;
             startX = targetX;
             startY = targetY;
         }
 
-        return totalDistance;
+        return totalDist;
     }
 
     int bfs(vector<vector<int>>& forest, int rows, int cols, int startX, int startY, int targetX, int targetY) {
@@ -54,23 +55,23 @@ public:
         distance[startX][startY] = 0;
 
         while (!q.empty()) {
-            pair<int, int> currentPosition = q.front();
+            pair<int, int> currentPos = q.front();
             q.pop();
 
-            if (currentPosition.first == targetX && currentPosition.second == targetY) {
+            if (currentPos.first == targetX && currentPos.second == targetY) {
                 return distance[targetX][targetY];
             }
 
-            for (int direction = 0; direction < 4; direction++) {
+            for (int dir = 0; dir < 4; dir++) {
 
-                int newX = currentPosition.first + dX[direction];
-                int newY = currentPosition.second + dY[direction];
+                int newX = currentPos.first + dX[dir];
+                int newY = currentPos.second + dY[dir];
 
                 if (newX < 0 || newX >= rows || newY < 0 || newY >= cols) continue;
                 if (forest[newX][newY] == 0) continue;
-                if (distance[newX][newY] != MAX_DISTANCE) continue;
+                if (distance[newX][newY] != MAX_D) continue;
 
-                distance[newX][newY] = distance[currentPosition.first][currentPosition.second] + 1;
+                distance[newX][newY] = distance[currentPos.first][currentPos.second] + 1;
                 q.push({newX, newY});
             }
         }
