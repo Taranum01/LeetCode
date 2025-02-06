ec7c8a7d5e3ebb65b8e89da55cfaa1e0003f2cea
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2025-02-06 10:39:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (89.34%) - LeetSync

---
+++ 768-partition-labels/partition-labels.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        
+        vector<int>last_occ(26,0);
+            for(int i=0; i<s.length(); i++)
+                last_occ[s[i]-'a']= i;
+        
+        vector<int>result;
+        int start=0;
+        int last=0;
+        for(int i=0; i<s.length(); i++){
+            last=max(last,last_occ[s[i]-'a']);
+        if(i==last) {
+            result.push_back(i-start+1);
+        start=i+1;
+        
+    }
+      }
+      
+      return result;
+        
+    }
+};
