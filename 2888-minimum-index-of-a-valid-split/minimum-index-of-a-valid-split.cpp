3617feb5bd2fd97f9e660ac9363b6b8760aee4b8
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2025-03-27 18:19:02 +0530

    Time: 4 ms (77.46%) | Memory: 89.9 MB (70.90%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -4,27 +4,46 @@ public:
 
         int n = nums.size();
 
-        unordered_map<int, int> mp1; 
-        unordered_map<int, int> mp2; 
-
-        for(int &num : nums) {
-            mp2[num]++;
-        }
+        int ele  = -1;
+        int count = 0;
 
         for(int i = 0; i < n; i++) {
-            int num = nums[i];
+            if(count == 0) {
+                ele = nums[i];
+                count = 1;
+            } else if(nums[i] == ele) {
+                count++;
+            } else {
+                count--;
+            }
+        }
 
-            mp1[num]++;
-            mp2[num]--;
+        int eleCount = 0;
+
+        for(int &num : nums) {
+            if(num == ele) {
+                eleCount++;
+            }
+        }
+
+        count = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == ele) {
+                count++;
+            }            
+            int remainingCount = eleCount - count;
 
             int n1 = i+1;
             int n2 = n-i-1;
 
-            if(mp1[num]*2 > n1 && mp2[num]*2 > n2) {
+            if(count*2 > n1 && remainingCount*2 > n2) {
                 return i;
             }
         }
 
+
         return -1;
+
     }
 };
