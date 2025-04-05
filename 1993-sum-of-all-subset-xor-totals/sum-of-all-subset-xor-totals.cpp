ea53ff31c80b5a020c34075c786b77ec9d07b8ab
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2025-04-05 16:10:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (71.99%) - LeetSync

---
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+
+    int solve(int i, vector<int>& nums, int Xor) {
+
+        if(i == nums.size()) {
+            return Xor;
+        }
+
+        int include = solve(i+1, nums, nums[i] ^ Xor);
+        int exclude = solve(i+1, nums, Xor);
+
+        return include + exclude;
+
+    }
+
+    int subsetXORSum(vector<int>& nums) {
+        
+        return solve(0, nums, 0);
+    }
+};
