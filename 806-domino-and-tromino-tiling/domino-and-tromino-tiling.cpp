534349c6eb8faa3a9d4db1d58ca9c7afacdd82fe
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2025-05-05 09:36:32 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (94.11%) - LeetSync

---
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    int M = 1000000007;
+    int dp[1001];
+
+    int solve(int n) {
+         if(n == 1 || n == 2)
+            return n;
+        if(n == 3)
+            return 5;
+        if(dp[n] != -1)
+            return dp[n];
+        
+        return dp[n] = (2*solve(n-1)%M + solve(n-3)%M)%M;
+    }
+    
+    int numTilings(int n) {
+       
+        memset(dp, -1, sizeof(dp));
+        return solve(n);
+        
+    }
+};
