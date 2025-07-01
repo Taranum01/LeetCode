3188994abfc1114591f7971c9b0e66d3f573048e
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2025-07-01 22:09:09 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (29.69%) - LeetSync

---
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+
+        int count = 0;
+
+        for(int i = 1; i < word.length(); i++) {
+            if(word[i] == word[i-1])
+                count++;
+        }
+
+        return count+1; 
+    }
+};
