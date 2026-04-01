1a2ec80a9368dca3fcf319d9880c70195a8db7c3
Taranum Wasu <81034301+Taranum01@users.noreply.github.com>
2026-04-01 15:43:20 +0530

    Time: 6 ms (66.48%) | Memory: 11.5 MB (81.49%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        int n=s.size(),l=0,r=0,ans=0;     
+        
+        vector<int> count(256,0);      
+        
+        while(r<n)      
+        {
+            count[s[r]]++;     
+            
+            while(count[s[r]]>1)      
+            {
+                count[s[l]]--;     
+                
+                l++;       
+            }
+            ans=max(ans,r-l+1);    
+            
+            r++;     
+        }
+        return ans;
+    }
+};
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+        
+//         vector<int> mp(256, -1); 
+        
+//         int left=0;
+//         int right=0;
+//         int n= s.size();
+//         int len=0;
+        
+//         while(right<n){
+//             if(mp[s[right]!= -1])
+//             left= max(mp[s[right]]+1,left);
+//             mp[s[right]]=right;
+//             len= max(len, right-left + 1);
+//             right++;
+//     }
+//     return len;
+//     }
+// };
+
